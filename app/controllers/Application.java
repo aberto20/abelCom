4b1c3c770c714147152c8531a57bdae721393906
aberto20 <aberto20@gmail.com>
2017-06-22 00:33:24 +0200

    jgv

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,5 +13,16 @@ public class Application extends Controller {
     public static Result dashboard(){
         return ok(dashboard.render());
     }
+    public static Result agentHome(){
+        return ok(agentHomepage.render());
+    }
+    public static Result provider(String provider) {
+         if(provider.equals("EUCL")){
+                return ok(eucl.render(provider));
+            }
+
+
+        return ok("Unknown service");
+    }
 
 }
--- public/js/index.js
+++ public/js/index.js
@@ -6,6 +6,47 @@
 });
 
  app.controller("maincontroller",function($scope){
+	 var s1 = {name: "Tigo Airtime", provider: "Tigo", active: true};
+	 var s2 = {name: "Tigo cash", provider: "Tigo", active: true};
+
+	 var s4 = {name: "MTN Airtime", provider: "MTN", active: true};
+	 var s5 = {name: "Mtn mobile money", provider: "MTN", active: true};
+
+	 var s6 = {name: "Prepaid electricity", provider: "EUCL", active: true};
+
+	 var s7 = {name: "Star time TV", provider: "Star time", active: true};
+
+	 var s8 = {name: "DSTV Booket", provider: "DSTV", active: true};
+	 var s9 = {name: "Canal Booket", provider: "DSTV", active: true};
+
+	 $scope.service = [];
+	 $scope.service[0] = {name: "Tigo", services: [s1, s2], active: true};
+	 $scope.service[1] = {name: "MTN", services: [s4, s5], active: true};
+	 $scope.service[2] = {name: "EUCL", services: [s6], active: true};
+	 $scope.service[3] = {name: "Star time", services: [s7], active: true};
+	 $scope.service[4] = {name: "DSTV", services: [s8, s9], active: true};
+
+	 $scope.selectedProvider = {};
+      console.log($scope.service);
+	 $scope.loadSelectedProvider = function(name){
+
+		 for(var p in $scope.service){
+			 if($scope.service[p].name == name){
+				 $scope.selectedProvider = $scope.service[p]
+			 }
+		 }
+
+	 };
+	 $scope.whichService = function (path) {
+		 var p = path.replaceAll(" ", "%20");
+		 if ($location.absUrl().indexOf(p) > -1) {
+			 //alert(path);
+			 return true;
+		 }
+
+		 return false;
+	 };
+
 	 Highcharts.chart('container', {
 		 chart: {
 			 type: 'line'
